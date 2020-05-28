6c2f822d776e0af2c01441882ad4ef31deffea1e
Huned Botee <24231+huned@users.noreply.github.com>
2020-05-28 14:14:35 -0700

    Implement asyncGzip.

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const zlib = require('zlib')
 
+const asyncGzip = async buffer => {
+  return new Promise((resolve, reject) => {
+    zlib.gzip(buffer, {}, (error, gzipped) => {
+      if (error) {
+        reject(error)
+      }
+      resolve(gzipped)
+    })
+  })
+}
+
 const asyncGunzip = async buffer => {
   return new Promise((resolve, reject) => {
     zlib.gunzip(buffer, {}, (error, gunzipped) => {
       if (error) {
         reject(error)
       }
-      resolve(gunzipped.toString())
+      resolve(gunzipped)
     })
   })
 }
 
-module.exports = asyncGunzip
+module.exports = {
+  gzip: asyncGzip,
+  asyncGunzip: asyncGunzip
+}
